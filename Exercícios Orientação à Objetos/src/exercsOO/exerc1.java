466694f2e5d1eefe11753ab4c7c84bc222147dd8
LucasBergholz <lucas.bergholz@gmail.com>
2022-06-09 11:26:40 -0300

    First Exercise Complete

--- Exercícios Orientação à Objetos/src/exercsOO/exerc1.java
+++ Exercícios Orientação à Objetos/src/exercsOO/exerc1.java
@@ -1,5 +1,36 @@
 package exercsOO;
 
+import java.util.Scanner;
+
 public class exerc1 {
+	
+	public static void main (String[] args) {
+		/*Program that calculates a driver's
+		  fine according to his speed;    */
+		
+		int maxSpeed, driverSpeed, fine;
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("What is the speed limit of the road?");
+		maxSpeed = input.nextInt();
+		System.out.println("What is the speed of the driver?");
+		driverSpeed = input.nextInt();
+		
+		if (driverSpeed > (maxSpeed + 30)) {
+			fine = 200;
+			
+		} else if (driverSpeed > (maxSpeed + 10)){
+			fine = 100;
+
+		} else if (driverSpeed > maxSpeed){
+			fine = 50;
+			
+		} else {
+			fine = 0;
+		}
+		
+		System.out.println("The value of the fine is = " + fine);
+		
+	}
 
 }
